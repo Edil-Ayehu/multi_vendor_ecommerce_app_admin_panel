43645e4137391c658704b009c9372ab45bf52043
Edilayehu Tadesse <edilayehu534027@gmail.com>
2024-11-21 15:57:10 -0800

    google font poppins added.

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
       useMaterial3: true,
       brightness: Brightness.light,
+      textTheme: GoogleFonts.poppinsTextTheme(ThemeData.light().textTheme),
       colorScheme: ColorScheme.light(
         primary: Colors.blue[700]!,
         secondary: Colors.purple[600]!,
@@ -22,12 +24,20 @@ class AppTheme {
         elevation: 0,
         backgroundColor: Colors.blue[700],
         foregroundColor: Colors.white,
+        titleTextStyle: GoogleFonts.poppins(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
           elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8),
+          ),
+          textStyle: GoogleFonts.poppins(
+            fontWeight: FontWeight.w500,
           ),
         ),
       ),
@@ -38,6 +48,7 @@ class AppTheme {
     return ThemeData(
       useMaterial3: true,
       brightness: Brightness.dark,
+      textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme),
       colorScheme: ColorScheme.dark(
         primary: Colors.blue[400]!,
         secondary: Colors.purple[300]!,
@@ -51,16 +62,24 @@ class AppTheme {
           borderRadius: BorderRadius.circular(12),
         ),
       ),
-      appBarTheme: const AppBarTheme(
+      appBarTheme: AppBarTheme(
         elevation: 0,
         backgroundColor: Colors.black,
         foregroundColor: Colors.white,
+        titleTextStyle: GoogleFonts.poppins(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
           elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8),
+          ),
+          textStyle: GoogleFonts.poppins(
+            fontWeight: FontWeight.w500,
           ),
         ),
       ),
